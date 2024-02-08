832902c6efaf2492d6bfa53e8d1f5389c238b0d4
turabayo <i.turabayo@alustudent.com>
2024-02-08 21:34:43 +0200

    isomo

--- lib/Repository/SearchItems.dart
+++ lib/Repository/SearchItems.dart
@@ -7,10 +7,19 @@ class Students {
 
 List<Students> studentDetails = [
   Students(name: "Amaryllis"),
+  Students(name: "Alpenrose"),
+  Students(name: "American Lotus"),
+  Students(name: "Aztec Lily"),
+  Students(name: "Amaryllis belladonna"),
+  Students(name: "Bachelors Button"),
+  Students(name: "Basket of Gold"),
+  Students(name: "Bee Balm Flower"),
   Students(name: "Begonia"),
+  Students(name: "Brazilian Plume"),
   Students(name: "Camelia"),
+  Students(name: "California Poppy"),
   Students(name: "Daffodil"),
+  Students(name: "Dahlias"),
+  Students(name: "Egyptian Blue Water Lily"),
   Students(name: "Everlasting Flower"),
-
-  
 ];
