f1144645d181ae5e21ffa900d088395247023ab4
LYCE899 <a.coulibaly@alustudent.com>
2024-03-10 05:53:23 +0200

    change

---
+++ lib/community.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class CommunityPage extends StatefulWidget {
+  @override
+  _CommunityPageState createState() => _CommunityPageState();
+}
+
+class _CommunityPageState extends State<CommunityPage> {
+  TextEditingController _commentController = TextEditingController();
+  List<String> _comments = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Our Community"),
+      ),
+      body: Stack(
+        children: [
+          // Background image
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/ISOMO_BACKGROUND.jpg"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  itemCount: _comments.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(_comments[index]),
+                    );
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: _commentController,
+                  decoration: InputDecoration(
+                    hintText: "Enter your comment...",
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _comments.add(_commentController.text);
+                    _commentController.clear();
+                  });
+                },
+                child: Text("POST"),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+
+import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
+import 'package:firebase_core/firebase_core.dart';
+
+import 'community.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: newMethod.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+dynamic get newMethod => DefaultFirebaseOptions;
+
+class DefaultFirebaseOptions {
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: newMethod(),
+      title: 'our community',
+      initialRoute: '/community',
+      routes: {
+        '/community': (context) =>  CommunityPage(),
+      },
+    );
+  }
+
+  dynamic newMethod() => HomeScreen();
+}
+
+class HomeScreen {
+}
